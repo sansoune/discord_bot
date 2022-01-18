265d9eca501d3ddbfed83f2a7aa2f3fec79b3dfa
sansoune <yassine.dakhlaoui06@gmail.com>
2022-01-18 22:49:46 +0100

    adding the command handler

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js')
 const dotenv = require('dotenv')
+const fs = require('fs')
+const path = require('path')
 dotenv.config()
 
 
@@ -13,6 +15,16 @@ const client = new Discord.Client({
     ]
 })
 
+client.commands = new Discord.Collection()
+
+
+//npm rebuild node-sass
+
+fs.readdirSync(path.resolve(__dirname, 'commands/')).filter(file => file.endsWith('.js')).forEach(file => {
+    const command = require(`./commands/${file}`)
+    console.log(`command ${command.name} has bean loaded `)
+    client.commands.set(command.name, command)
+})
 
 
 
@@ -23,9 +35,16 @@ client.on('ready', () => {
 })
 
 client.on('messageCreate', (message) => {
-    if (message.content === 'ping') {
-        message.reply({content: "pong"})
-    }
+    const prefix = 'nano'
+    if (!message.content.startsWith(prefix)) return;
+    const args = message.content.substring(prefix.length + 1).split(/ +/)
+    console.log(args)
+    const command = client.commands.find(cmd => cmd.name === args[0])
+
+    if(!command) return message.reply("this command does not exist")
+
+    command.run(message, args, client)
+        
 })
 
 
